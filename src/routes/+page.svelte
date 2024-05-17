c7a8ba68d436dfbfdfb195c6ac69fdc60ddd1f9b
junnie <lommy101@gmail.com>
2024-05-17 15:38:55 +0900

    tailwindcss init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,13 @@
 </svelte:head>
 
 <section>
-Home
+	<h1 class="text-3xl font-bold underline">
+		Home
+	</h1>
 </section>
 
-<style>
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
