2bec13511854fbdb55ebe07213c83bd8c4b2713a
junnie <lommy101@gmail.com>
2024-05-29 23:01:15 +0900

    save

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:sulchanggo/login/login.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -53,6 +54,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const ShadInput(
+          placeholder: Text('Search'),
+          suffix: Padding(
+            padding: EdgeInsets.all(4.0),
+            child: ShadImage.square(size: 18, LucideIcons.search),
+          ),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -63,13 +73,81 @@ class _HomePageState extends State<HomePage> {
                       itemCount: data.length,
                       itemBuilder: (context, index) {
                         final item = data[index];
-                        return ListTile(
-                          leading: Image.network(
-                            'https://picsum.photos/200',
-                            fit: BoxFit.cover,
+                        final DateTime createdAt =
+                            DateTime.parse(item['created_at']);
+                        final String formattedDate =
+                            DateFormat('yyyy-MM-dd HH:mm:ss').format(createdAt);
+                        final bool isWithinOneMonth =
+                            DateTime.now().difference(createdAt).inDays <= 30;
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                            right: 8.0,
+                            top: 4.0,
+                            bottom: 4.0,
                           ),
-                          title: Text(item['name'].toString()),
-                          subtitle: Text(item['ml'].toString()),
+                          child: ShadCard(
+                            content: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          if (isWithinOneMonth)
+                                            const ShadBadge.destructive(
+                                              text: Text('New'),
+                                            ),
+                                          const SizedBox(width: 8),
+                                          Text(
+                                            item['name'].toString(),
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 8),
+                                      Text(
+                                        "용량: ${item['ml'].toString()}ml",
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 6),
+                                      Text(
+                                        "도수: ${item['alcohol'].toString()}%",
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 6),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(width: 16),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    'https://picsum.photos/100',
+                                    width: 100,
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            footer: Text(
+                              formattedDate,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
                         );
                       },
                     ),
